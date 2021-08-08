e0cb11a70ebc23a3ad2fc426e44ff8eda2574884
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2021-08-08 20:48:16 +0700

    Perubahan main

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -104,7 +104,7 @@ const Todo = (props) => {
         <div className="row justify-content-center">
           <div className="col-md-8">
             <h1 className={styles.title}>
-              Todo App BranchBaru
+              Todo App Master
             </h1>  
           </div>
         </div>
