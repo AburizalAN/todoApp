5f473adc6d8e8bfba5a064725d871cf708710545
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2021-08-08 22:11:31 +0700

    ke branch Baru

--- src/pages/Todo/index.js
+++ src/pages/Todo/index.js
@@ -104,7 +104,7 @@ const Todo = (props) => {
         <div className="row justify-content-center">
           <div className="col-md-8">
             <h1 className={styles.title}>
-              Todo App Main ini
+              Todo App Main ini mau ke branch baru
             </h1>  
           </div>
         </div>
